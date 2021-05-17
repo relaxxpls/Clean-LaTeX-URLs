3ee11d73675c26d476ca320a2b150e109970510b
Laxman Desai <lolopaw24@gmail.com>
2021-05-18 05:29:13 +0530

    🐛 FIX: Add file IO functionality

--- src/main.py
+++ src/main.py
@@ -8,17 +8,19 @@ import sys
 parser = ArgumentParser(description='URL support for LaTeX')
 
 # Positional arguments
-parser.add_argument('input', type=Path, help='Input file')
+parser.add_argument('input', type=Path, help='Input file.')
 
 # Optional arguments
-parser.add_argument('--output', '-o', type=Path, help='Output file')
+parser.add_argument('--output', '-o', type=Path, help='Output file.')
 parser.add_argument('--dump', '-d', type=Path,
                     help='Folder to store the downloaded content.')
-parser.add_argument('--all', '-a', action='store_true',
-                    help='Flag to download every URL present')
 parser.add_argument('--tag', '-t', type=str, nargs='+',
                     default=["includegraphics", "url"],
-                    help='LaTeX tags that contain required URLs')
+                    help='LaTeX tags that contain required URLs.')
+parser.add_argument('--all', '-a', action='store_true',
+                    help='Flag to download every URL present.')
+parser.add_argument('--force', '-f', action='store_const', default='x',
+                    const='w', help='Force overwrite if file already exists.')
 
 args = parser.parse_args()
 
@@ -28,8 +30,9 @@ INPUT_FILE_NAME = INPUT_FILE.name
 INPUT_FILE_STEM = INPUT_FILE.stem
 OUTPUT_FILE = BASE_DIR / f'{INPUT_FILE_STEM} TeXURL_out.tex'
 DUMP_DIR = BASE_DIR / '.TeXURL_dump/'
-ALL = args.all
 TAGS = args.tag
+ALL = args.all
+FORCE = args.force
 
 if INPUT_FILE.is_file() == False:
     raise FileNotFoundError(
@@ -63,10 +66,20 @@ if not ALL:
 
 # ----------- download & link
 try:
-    with open(INPUT_FILE) as f:
-        text = f.read()
+    with open(INPUT_FILE, 'r') as f:
+        lines = f.readlines()
     
-    urls = url_regex.findall(text)
+    # with open(OUTPUT_FILE, 'x') as f:
+    # FORCE = 'x' if not FORCE else 'w'
+    with open(OUTPUT_FILE, FORCE) as f:
+        for line in lines:
+            urls = url_regex.findall(line)
+            if urls:
+                # Process the URL here
+                pass
+            print(line, end='', file=f)
 
-except IOError:
-    print('No file named "%s" was found at "%s"' % (INPUT_FILE_NAME, BASE_DIR))
+except OSError as e:
+    print(f'I/O error #{e.errno}: {e.strerror}')
+except:
+   print(f'Unexpected error: {sys.exc_info()[0]}')
