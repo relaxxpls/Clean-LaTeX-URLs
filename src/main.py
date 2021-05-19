71a018907140ba031dde02fd21de473f01b4ebe8
Laxman Desai <lolopaw24@gmail.com>
2021-05-19 23:50:57 +0530

    🐛 FIX: Package works! (only with -a flag)

--- src/main.py
+++ src/main.py
@@ -5,21 +5,20 @@ import requests
 import sys
 import hashlib
 
-# ! Add support for --output given as filename to be valid
+
 # ? Provides commandline interface support for the script
+# ! Add support for --output given as filename to be valid
+
 def parse():
     parser = ArgumentParser(description='URL support for LaTeX')
     parser.add_argument('input', type=Path, help='Input file.')
     parser.add_argument('--output', '-o', type=Path, help='Output file.')
-    parser.add_argument('--dump', '-d', type=Path,
-                        help='Folder to store the downloaded content.')
-    parser.add_argument('--tag', '-t', type=str, nargs='+',
-                        default=["includegraphics", "url"],
+    parser.add_argument('--dump', '-d', type=Path, help='Folder to store the downloaded content.')
+    parser.add_argument('--tag', '-t', type=str, nargs='+', default=["includegraphics", "url"],
                         help='LaTeX tags that contain required URLs.')
-    parser.add_argument('--all', '-a', action='store_true',
-                        help='Flag to download every URL present.')
-    parser.add_argument('--force', '-f', action='store_const', default='x',
-                        const='w', help='Force overwrite if file already exists.')
+    parser.add_argument('--all', '-a', action='store_true', help='Flag to download every URL present.')
+    parser.add_argument('--force', '-f', action='store_const', default='x', const='w',
+                        help='Force overwrite if file already exists.')
     args = parser.parse_args()
 
     INPUT_FILE = args.input.resolve()
@@ -30,16 +29,14 @@ def parse():
     try:
         open(INPUT_FILE)
     except:
-        print(f'{sys.exc_info()[0].__name__}: {sys.exc_info()[1]}')
-        sys.exit()
+        raise SystemExit(f'{sys.exc_info()[0].__name__}: {sys.exc_info()[1]}')
 
     # ? Use the user's output file (if valid)
     if args.output is not None:
         if args.output.resolve().is_file():
             OUTPUT_FILE = args.output
         else:
-            print(f'"{args.output.resolve()}" is not a valid file (or file name).',
-                  file=sys.stderr)
+            print(f'"{args.output.resolve()}" is not a valid file (or file name).', file=sys.stderr)
             print('Switching to the default instead.', file=sys.stderr)
             print('Default = "{OUTPUT_FILE}")', file=sys.stderr)
 
@@ -48,45 +45,84 @@ def parse():
         if args.dump.resolve().is_dir():
             DUMP_DIR = args.dump
         else:
-            print(f'"{args.dump.resolve()}" is not a valid directory.',
-                  file=sys.stderr)
+            print(f'"{args.dump.resolve()}" is not a valid directory.', file=sys.stderr)
             print('Switching to the default instead.', file=sys.stderr)
-            print('Default = "{DUMP_DIR}")', file=sys.stderr)
+            print(f'Default = "{DUMP_DIR}")', file=sys.stderr)
 
     return INPUT_FILE, OUTPUT_FILE, DUMP_DIR, args.force, args.all, args.tag
 
 
 # ? Returns the regex to search for URLs
-def get_regex(ALL, TAGS):
-    url_regex = re.compile('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+
+def get_regex(TAGS, ALL=False):
+    url_regex = re.compile(
+        r'(?P<url>http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)')
     if not ALL:
         tags_merged = '|'.join(TAGS)
-        url_regex = re.compile(r'\\(?:%s){(?P<url>[^}]+)}' % tags_merged)
+        url_regex = re.compile(r'(?:\\(?:%s){(?P<url>[^}]+)})' % tags_merged)
     return url_regex
+
+
+# ! 1. https://stackoverflow.com/a/16511493/14493047: Check if the user is online
+# ? Checks if url passed is downloadable
+
+def chk_url(url):
+    ignore_content_types = ['text/html', 'text', 'html']
+    try:
+        head = requests.head(url, allow_redirects=True, timeout=5)
+        if head.status_code == 200:
+            content_type = head.headers.get('content-type').lower()
+            for wrong_type in ignore_content_types:
+                if wrong_type in content_type:
+                    print(f'Skipping "{url}" as content-type = "{content_type}"')
+                    return False
+        else:
+            print(f'Skipping "{url}" as HTTP status code "{head.status_code}"')
+            return False
+        return True
+    except:
+        raise SystemExit(f'{sys.exc_info()[0].__name__}: {sys.exc_info()[1]}')
+
+
+# ! Changes: MD5 hash the urls to get filename
+# ? Download content and link it
+# * Download it ONLY if we hadn't previously downloaded it
+
+def download_and_link(url_match, directory):
+    url = url_match.group('url')
+    try:
+        download_name = url.split('/')[-1]
+        download_file = directory / download_name
+        if not download_file.is_file():
+            if not chk_url(url):
+                return url
+            r = requests.get(url, allow_redirects=True, timeout=5)
+            if r.status_code == 200:
+                with open(directory/download_file, 'wb') as f:
+                    f.write(r.content)
+            else:
+                print(f'Skipping "{url}" as HTTP status code {r.status_code}')
+        else:
+            print(f'Skipping "{url}" is content already downloaded.')
+    except:
+        raise SystemExit(f'{sys.exc_info()[0].__name__}: {sys.exc_info()[1]}')
+    return str(download_file)
 
 
 # ? Download content and link it
 def main():
     INPUT_FILE, OUTPUT_FILE, DUMP_DIR, FORCE, ALL, TAGS = parse()
-    url_regex = get_regex(ALL, TAGS)
-
     try:
-        with open(INPUT_FILE, 'r') as f:
-            lines = f.readlines()
         DUMP_DIR.mkdir(parents=True, exist_ok=True)
-
+        with open(INPUT_FILE, 'r') as f:
+            text = f.read()
         with open(OUTPUT_FILE, FORCE) as f:
-            for line in lines:
-                urls = url_regex.findall(line)
-                for url in urls:
-                    download_name = url.split('/')[-1]
-                    with requests.get(url, stream=True) as r:
-                        print(DUMP_DIR / download_name)
-                print(line, end='', file=f)
-
+            url_regex = get_regex(TAGS, ALL)
+            linked_text = url_regex.sub(repl=lambda x: download_and_link(x, DUMP_DIR), string=text)
+            f.write(linked_text)
     except:
-        print(f'{sys.exc_info()[0].__name__}: {sys.exc_info()[1]}')
-        sys.exit()
+        raise SystemExit(f'{sys.exc_info()[0].__name__}: {sys.exc_info()[1]}')
+
 
 if __name__ == '__main__':
     main()
